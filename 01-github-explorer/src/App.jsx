8225703b377f1de0c05c2d75a86a1619c8ba010b
rafael.braga <rafael.braga.queiroz@gmail.com>
2021-09-08 22:41:54 -0300

    add small change

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,5 +1,11 @@
-import { RepositoryList } from './components/RepositoryList'
-import './styles/global.scss'
+import { Counter } from "./components/Counter";
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 export function App() {
-    return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
